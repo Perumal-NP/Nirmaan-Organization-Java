3b897760175058218180ddac3d5f090b875f4caa
Perumal N <148893073+Perumal-NP@users.noreply.github.com>
2025-04-03 15:37:45 +0530

    Day2

---
+++ VariableTask.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class VariableTask {
+public static void main(String[] args) {
+	byte b=18;
+	short s=20000;
+	int num=34567577;
+	long l=638274554l;
+	float digit=2.14f;
+	double digitValueDouble=568679.78978976876889;
+	boolean isTrue=true;
+	char c='N';
+	System.out.println(b);
+	System.out.println(s);
+	System.out.println(num);
+	System.out.println(l);
+	System.out.println(digit);
+	System.out.println(digitValueDouble);
+	System.out.println(isTrue);
+	System.out.println(c);
+	
+	
+}
+}
