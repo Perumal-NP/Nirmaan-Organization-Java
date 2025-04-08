f26729d634d2f6f1527bd9a579601435b686166f
Perumal N <148893073+Perumal-NP@users.noreply.github.com>
2025-04-08 10:17:13 +0530

    day3

---
+++ VarAndOprator.java
@@ -0,0 +1,15 @@
+package day3;
+
+import java.util.Scanner;
+
+public class VarAndOprator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String name=sc.next();
+		System.out.println(name);
+
+	}
+
+}
