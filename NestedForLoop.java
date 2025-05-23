ef7b208cd688c8ff0c3d3012c6901b270395dd8f
Perumal N <148893073+Perumal-NP@users.noreply.github.com>
2025-05-23 11:35:49 +0530

    day7

---
+++ NestedForLoop.java
@@ -0,0 +1,21 @@
+package day7;
+
+public class NestedForLoop {
+public static void main(String [] args) {
+	for(int i=0;i<=10;i++) {
+
+          System.out.println(" ");
+          for(int k=10;i<=k;k--) {
+        	  System.out.print(" ");
+        	  
+          }
+		for(int j=0;j<=i;j++) {
+		   
+			System.out.print("$");
+			System.out.print(" ");
+		}
+	
+	}
+}
+}
+ 
